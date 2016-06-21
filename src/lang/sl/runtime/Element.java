78b6caeed321a7a1b5e6444f746d541185d983ce
vrozen <R.A.van.Rozen@cwi.nl>
2016-06-21 17:38:30 +0200

    fixed migration

--- src/lang/sl/runtime/Element.java
+++
@@ -1,6 +0,0 @@
-package lang.sl.runtime;
-
-public abstract class Element
-{
-	public String id;
-}
--- src/lang/sl/runtime/Group.java
+++
@@ -1,9 +0,0 @@
-package lang.sl.runtime;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class Group extends Element
-{
-  public List<Element> states = new ArrayList<Element>();
-}
